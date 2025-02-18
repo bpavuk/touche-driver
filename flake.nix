b8514abe9ea4ea85a8f0e4ecf98ebb042a9f40ae
bpavuk <boulderobscura@gmail.com>
2025-02-18 13:36:01 +0200

    nix build can now build a driver executable

--- flake.nix
+++ flake.nix
@@ -8,22 +8,37 @@
         nixpkgs.follows = "nixpkgs";
       };
     };
+    crane = {
+      url = "github:ipetkov/crane";
+    };
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-	rustToolchain = pkgs.pkgsBuildHost.rust-bin.stable.latest.default;
-	nativeBuildInputs = with pkgs; [ rustToolchain pkg-config gh ];
-	buildInputs = with pkgs; [ libudev-zero ];
+        rustToolchain = pkgs.pkgsBuildHost.rust-bin.stable.latest.default;
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+
+        src = craneLib.cleanCargoSource ./.;
+        nativeBuildInputs = with pkgs; [ rustToolchain pkg-config gh ];
+        buildInputs = with pkgs; [ libudev-zero ];
+        commonArgs = {
+          inherit src buildInputs nativeBuildInputs;
+        };
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        bin = craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
       in
       with pkgs;
       {
+        packages = {
+          inherit bin;
+          default = bin;
+        };
         devShells.default = mkShell {
-	  inherit buildInputs nativeBuildInputs;
+          inherit buildInputs nativeBuildInputs;
         };
       }
     );
