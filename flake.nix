20f0b87a919f11bcdb35be3e14fa7f3ee2b6f3e5
bpavuk <boulderobscura@gmail.com>
2026-07-30 09:45:47 +0300

    erase the past, restore the future

--- flake.nix
+++ flake.nix
@@ -2,49 +2,27 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-    crane = {
-      url = "github:ipetkov/crane";
-    };
+    zig-overlay.url = "github:mitchellh/zig-overlay";
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }:
+  outputs = { self, nixpkgs, flake-utils, zig-overlay }:
     flake-utils.lib.eachSystem [ flake-utils.lib.system.x86_64-linux flake-utils.lib.system.aarch64-linux ] (system:
       let
-        overlays = [ (import rust-overlay) ];
+        overlays = [ zig-overlay.overlays.default ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        rustToolchain = pkgs.pkgsBuildHost.rust-bin.stable.latest.default;
-        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
-
-        src = craneLib.cleanCargoSource ./.;
-        nativeBuildInputs = with pkgs; [ rustToolchain rust-analyzer pkg-config ];
-        buildInputs = with pkgs; [ systemd android-udev-rules ];
-        commonArgs = {
-          inherit src buildInputs nativeBuildInputs;
-        };
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-        bin = craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
+        nativeBuildInputs = with pkgs; [ pkg-config zigpkgs."0.16.0" zls ];
+        buildInputs = with pkgs; [ systemd ];
       in
       with pkgs;
       {
         packages = {
-          inherit bin;
-          default = bin;
+          # inherit bin;
+          # default = bin;
         };
         devShells.default = mkShell {
           buildInputs = buildInputs;
-          nativeBuildInputs = nativeBuildInputs ++ (with pkgs; [
-            gh
-            neovim
-            lazygit
-            ripgrep
-          ]);
+          nativeBuildInputs = nativeBuildInputs;
         };
       }
     );
--- touche-lib/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod aoa;
-pub mod data;
-pub mod devices;
-pub mod driver;
-
-pub use touche_aoa;
